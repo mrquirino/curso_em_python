e4920c28deec007ebbbfd19d3c4659c189a72d0f
Matheus Quirino <matheusf730@gmail.com>
2025-08-20 10:38:32 -0300

    Aula 1

---
+++ Aula01/inicio.py
@@ -0,0 +1,18 @@
+#Declarando três variáveis
+nome = ""
+idade = ""
+local = ""
+
+#Solicitando informações
+print("Digite o seu nome: ")
+nome = input()
+print("Digite a sua idade: ")
+idade = input()
+print("Digite o seu estado: ")
+local = input()
+
+# Escrevendo e concatenado variáveis
+print("Seu nome é: ")
+print(nome)
+print("Sua idade é: " + idade)
+print("Seu estado de moradia é: " + local)
---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+opcao = ""
+
+print("1. Opção 1")
+print("2. Opção 2")
+print("3. Opção 3")
+print("4. Opção 4")
+opcao = input()
+print("Você escolheu: " + opcao)
